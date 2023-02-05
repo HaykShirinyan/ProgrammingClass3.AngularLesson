606300a2e866511230bb3c75f768a089caac814c
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2023-02-05 12:45:15 -0800

    Added products controller

---
+++ ProgrammingClass3.AngularLesson/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass3.AngularLesson.Data;
+using ProgrammingClass3.AngularLesson.Models;
+
+namespace ProgrammingClass3.AngularLesson.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ProductsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = _dbContext.Products.ToList();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product= _dbContext.Products.Find(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest("ID in the request body must be equal to ID in the URL.");
+            }
+
+            _dbContext.Products.Update(product);
+            _dbContext.SaveChanges();
+
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _dbContext.Products.Find(id);
+
+            if (product != null)
+            {
+                _dbContext.Products.Remove(product);
+                _dbContext.SaveChanges();
+
+                return Ok(product);
+            }
+
+            return NotFound();
+        }
+    }
+}
