b668081067986d7ac271ef7d78845dc5a1b51cc7
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-02-08 12:51:16 +0400

    add controllers

---
+++ ProgrammingClass3.AngularLesson/Controllers/ProductTypesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass3.AngularLesson.Data;
+using ProgrammingClass3.AngularLesson.Models;
+
+namespace ProgrammingClass3.AngularLesson.Controllers
+{
+    [Route("api/[productTypes]")]
+    [ApiController]
+    public class ProductTypesController : ControllerBase
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ProductTypesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProductTypes() 
+        {
+            var productTypes = _dbContext.ProductTypes.ToList();
+
+            return Ok(productTypes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductTypes(int id) 
+        {
+            var productType = _dbContext.ProductTypes.Find(id);
+
+            if(productType == null) 
+            {
+                return NotFound();
+            }
+
+            return Ok(productType);
+        }
+
+    }
+}
