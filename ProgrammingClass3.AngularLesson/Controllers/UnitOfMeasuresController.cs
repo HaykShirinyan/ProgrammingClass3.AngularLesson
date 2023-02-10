2c509f88f11d930479ec565dd6fc2aea54841683
martzuro <arturgevorgyan1131@gmail.com>
2023-02-11 00:21:38 +0400

    add ProductType and UnitOfMeasure Models and  api controllers

---
+++ ProgrammingClass3.AngularLesson/Controllers/UnitOfMeasuresController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass3.AngularLesson.Data;
+using ProgrammingClass3.AngularLesson.Data.Migrations;
+using ProgrammingClass3.AngularLesson.Models;
+
+namespace ProgrammingClass3.AngularLesson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UnitOfMeasuresController : ControllerBase
+    {
+        private ApplicationDbContext _dbContext;
+
+        public UnitOfMeasuresController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.ToList();
+
+            return Ok(unitOfMeasure);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.Find(id);
+
+            if (unitOfMeasure == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(unitOfMeasure);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(UnitOfMeasure unitOfMeasure)
+        {
+            _dbContext.UnitOfMeasures.Add(unitOfMeasure);
+            _dbContext.SaveChanges();
+
+            return Ok(unitOfMeasure);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, UnitOfMeasure unitOfMeasure)
+        {
+            if (id != unitOfMeasure.Id)
+            {
+                return BadRequest("ID in the request body must be equal to ID in the URL.");
+            }
+
+            _dbContext.UnitOfMeasures.Update(unitOfMeasure);
+            _dbContext.SaveChanges();
+
+            return Ok(unitOfMeasure);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.Find(id);
+
+            if (unitOfMeasure != null)
+            {
+                _dbContext.UnitOfMeasures.Remove(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return Ok(unitOfMeasure);
+            }
+
+            return NotFound();
+        }
+    }
+}
--- ProgrammingClass3.AngularLesson/Data/Migrations/20230210191057_ProductTypes.Designer.cs
+++ ProgrammingClass3.AngularLesson/Data/Migrations/20230210191057_ProductTypes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProgrammingClass3.AngularLesson.Data;
 
@@ -11,9 +12,10 @@ using ProgrammingClass3.AngularLesson.Data;
 namespace ProgrammingClass3.AngularLesson.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230210191057_ProductTypes")]
+    partial class ProductTypes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -413,28 +415,6 @@ namespace ProgrammingClass3.AngularLesson.Data.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("ProductTypes");
-                });
-
-            modelBuilder.Entity("ProgrammingClass3.AngularLesson.Models.UnitOfMeasure", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                    b.Property<string>("Description")
-                        .HasMaxLength(500)
-                        .HasColumnType("nvarchar(500)");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasMaxLength(100)
-                        .HasColumnType("nvarchar(100)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("UnitOfMeasures");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
