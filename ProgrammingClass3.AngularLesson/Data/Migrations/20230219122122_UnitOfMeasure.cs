29d8cb3af35536c749fe4bfc85e6c2505e9b387c
User <User@DESKTOP-B9VU8GA>
2023-02-19 16:24:04 +0400

    AplicationDbContext and Migrations

---
+++ ProgrammingClass3.AngularLesson/Data/Migrations/20230219122122_UnitOfMeasure.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProgrammingClass3.AngularLesson.Data.Migrations
+{
+    public partial class UnitOfMeasure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
