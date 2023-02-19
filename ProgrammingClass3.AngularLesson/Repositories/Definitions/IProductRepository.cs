ff73167818de05d12e7e9727e4de23e587eb0140
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2023-02-19 12:30:57 -0800

    Added ProductRepository in C# and ProductService in Angular

---
+++ ProgrammingClass3.AngularLesson/Repositories/Definitions/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProgrammingClass3.AngularLesson.Models;
+
+namespace ProgrammingClass3.AngularLesson.Repositories.Definitions
+{
+    public interface IProductRepository
+    {
+        List<Product> GetAll();
+
+        Product Get(int id);
+
+        Product Add(Product product);
+
+        Product Update(Product product);
+
+        Product Delete(int id);
+    }
+}
---
+++ ProgrammingClass3.AngularLesson/Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using ProgrammingClass3.AngularLesson.Data;
+using ProgrammingClass3.AngularLesson.Models;
+using ProgrammingClass3.AngularLesson.Repositories.Definitions;
+
+namespace ProgrammingClass3.AngularLesson.Repositories.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        private ApplicationDbContext _dbContext;
+
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }   
+
+        public List<Product> GetAll()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public Product Get(int id)
+        {
+            return _dbContext.Products.Find(id);
+        }
+
+        public Product Add(Product product)
+        {
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+
+            return product;
+        }
+
+        public Product Update(Product product)
+        {
+            _dbContext.Products.Update(product);
+            _dbContext.SaveChanges();
+
+            return product;
+        }
+
+        public Product Delete(int id)
+        {
+            var product = _dbContext.Products.Find(id);
+
+            if (product != null)
+            {
+                _dbContext.Products.Remove(product);
+                _dbContext.SaveChanges();
+
+                return product;
+            }
+
+            return null;
+        }
+    }
+}
