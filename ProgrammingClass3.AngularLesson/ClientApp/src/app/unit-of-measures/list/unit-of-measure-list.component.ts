25a675823c26ea29b11a1b4a1da63db670db5fec
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-02-21 23:24:09 +0400

    add services

--- ProgrammingClass3.AngularLesson/ClientApp/src/app/unit-of-measures/list/unit-of-measure-list.component.ts
+++ ProgrammingClass3.AngularLesson/ClientApp/src/app/unit-of-measures/list/unit-of-measure-list.component.ts
@@ -1,21 +1,22 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { UnitOfMeasure } from "../../shared/models/unit-of-measure";
+import { UnitOfMeasureService } from "../../shared/services/unit-of-measure.service";
 
 @Component({
   templateUrl: '/unit-of-measure-list.component.html'
 })
 export class UnitOfMeasureListComponent implements OnInit {
-  private _httpClient: HttpClient;
+  private _unitOfMeasureService: UnitOfMeasureService;
 
   public unitOfMeasures?: UnitOfMeasure[];
 
-  constructor(httpClient: HttpClient) {
-    this._httpClient = httpClient;
+  constructor(unitOfMeasureService: UnitOfMeasureService) {
+    this._unitOfMeasureService = unitOfMeasureService;
   }
 
   public ngOnInit(): void {
-    this._httpClient.get<UnitOfMeasure[]>('api/unit-of-measures')
+    this._unitOfMeasureService.getAll()
       .subscribe(unitOfMeasures => {
         this.unitOfMeasures = unitOfMeasures
       })
