d37b7897ef0cc266efce5647fe3e5b1d876987da
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-03-01 11:29:14 +0400

    update create for productTypes

--- ProgrammingClass3.AngularLesson/ClientApp/src/app/product-types/create/create-product-type.component.ts
+++ ProgrammingClass3.AngularLesson/ClientApp/src/app/product-types/create/create-product-type.component.ts
@@ -1,13 +1,16 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { ProductType } from "../../shared/models/product-type";
 import { ProductTypeService } from "../../shared/services/product-type.service";
 
 @Component({
-  templateUrl: '.create-product-type.component.html'
+  templateUrl: './create-product-type.component.html'
 })
 export class CreateProductTypeComponent {
   private readonly _productTypeService: ProductTypeService;
   private readonly _router: Router;
+
+  public productType: ProductType = {};
 
   constructor(
     productTypeService: ProductTypeService,
