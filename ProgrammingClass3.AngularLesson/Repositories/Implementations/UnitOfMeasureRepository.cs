7faea893045cfd154cdbbe350def0a493a2b5d48
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-03-02 11:13:01 +0400

    Revert "Revert "add unitOfMeasureRepository""
    This reverts commit 4de428a8e5c3cfff9204effafbb74bc8f9b2041a.

---
+++ ProgrammingClass3.AngularLesson/Repositories/Implementations/UnitOfMeasureRepository.cs
@@ -0,0 +1,56 @@
+﻿using ProgrammingClass3.AngularLesson.Data;
+using ProgrammingClass3.AngularLesson.Models;
+using ProgrammingClass3.AngularLesson.Repositories.Definitions;
+
+namespace ProgrammingClass3.AngularLesson.Repositories.Implementations
+{
+    public class UnitOfMeasureRepository: IUnitOfMeasureRepository
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public UnitOfMeasureRepository(ApplicationDBContext dBContext) 
+        {
+            _dbContext = dBContext;
+        }
+
+        public List<UnitOfMeasure> GetAll()
+        {
+            return _dbContext.UnitOfMeasures.ToList();
+        }
+
+        public UnitOfMeasure Get(int id)
+        {
+            return _dbContext.UnitOfMeasures.Find(id);
+        }
+
+        public UnitOfMeasure Add(UnitOfMeasure unitOfMeasure)
+        {
+            _dbContext.UnitOfMeasures.Add(unitOfMeasure);
+            _dbContext.SaveChanges();
+
+            return unitOfMeasure;
+        }
+
+        public UnitOfMeasure Update(UnitOfMeasure unitOfMeasure)
+        {
+            _dbContext.UnitOfMeasures.Update(unitOfMeasure);
+            _dbContext.SaveChanges();
+
+            return unitOfMeasure;
+        }
+
+        public UnitOfMeasure Delete(int id)
+        {
+            var unitOfMeasure = _dbContext.UnitOfMeasures.Find(id);
+            if(unitOfMeasure != null)
+            {
+                _dbContext.UnitOfMeasures.Remove(unitOfMeasure);
+                _dbContext.SaveChanges();
+
+                return unitOfMeasure;
+            }
+
+            return null;
+        }
+    }
+}
