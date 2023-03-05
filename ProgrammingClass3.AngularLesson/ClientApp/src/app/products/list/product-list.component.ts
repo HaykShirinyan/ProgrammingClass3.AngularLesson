d9dc418b6e343e84969c9c8e20ce34bc3b5bd475
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2023-03-05 12:12:42 -0800

    Added loading spinner

--- ProgrammingClass3.AngularLesson/ClientApp/src/app/products/list/product-list.component.ts
+++ ProgrammingClass3.AngularLesson/ClientApp/src/app/products/list/product-list.component.ts
@@ -9,15 +9,23 @@ export class ProductListComponent implements OnInit {
   private _productService: ProductService;
 
   public products?: Product[];
+  public isLoading: boolean = false;
 
   constructor(productService: ProductService) {
     this._productService = productService;
   }
 
   public ngOnInit(): void {
+    this.isLoading = true;
+
     this._productService.getAll()
       .subscribe(products => {
         this.products = products;
+        this.isLoading = false;
       })
+  }
+
+  public cancelLoading(): void {
+    this.isLoading = false;
   }
 }
