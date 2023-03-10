5a3c31904b30b2cdf7b1e2b44ad8764258135888
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-03-11 01:57:37 +0400

    still working

---
+++ ProgrammingClass3.AngularLesson/ClientApp/src/app/shared/components/back-button/back-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+import { Router, RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-back-button',
+  templateUrl: './back-button.component.html'
+})
+export class BackButtonComponent {
+  @Input()
+  public buttonText: string = 'Go back';
+
+ 
+}
