e21bb4765bb0eccaf630d7b01a29278cba231cc7
Davit Aleyan <davaleyan91@gmail.com>
2023-03-11 18:59:28 +0400

    save

--- ProgrammingClass3.AngularLesson/ClientApp/src/app/products/list/product-list.component.ts
+++ ProgrammingClass3.AngularLesson/ClientApp/src/app/products/list/product-list.component.ts
@@ -1,23 +1,23 @@
-import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { Product } from "../../shared/models/product";
+import { ProductService } from "../../shared/services/product.service";
 
 @Component({
   templateUrl: './product-list.component.html'
 })
 export class ProductListComponent implements OnInit {
-  private _httpClient: HttpClient;
+  private _productService: ProductService;
 
   public products?: Product[];
 
-  constructor(httpClient: HttpClient) {
-    this._httpClient = httpClient;
+  constructor(productService: ProductService) {
+    this._productService = productService 
   }
 
   public ngOnInit(): void {
-    this._httpClient.get<Product[]>('api/products')
+    this._productService.getAll()
       .subscribe(products => {
         this.products = products;
-      });
+      })
   }
 }
