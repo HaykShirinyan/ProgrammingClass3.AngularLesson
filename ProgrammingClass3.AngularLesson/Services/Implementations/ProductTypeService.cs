d6481e6df6f07609d74586ccb2cde8a7fba1cc57
LEVON ADAMYAN <levon.adamyan23@gmail.com>
2023-03-19 01:11:48 +0400

    add services

---
+++ ProgrammingClass3.AngularLesson/Services/Implementations/ProductTypeService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using ProgrammingClass3.AngularLesson.DataTransferObjects;
+using ProgrammingClass3.AngularLesson.Models;
+using ProgrammingClass3.AngularLesson.Repositories.Definitions;
+
+namespace ProgrammingClass3.AngularLesson.Services.Implementations
+{
+    public class ProductTypeService
+    {
+        private readonly IMapper _mapper;
+        private readonly IProductTypeRepository _productTypeRepository;
+
+        public ProductTypeService(IMapper mapper, IProductTypeRepository productTypeRepository)
+        {
+            _mapper = mapper;
+            _productTypeRepository = productTypeRepository;
+        }
+
+        public List<ProductTypeDto> GetAll() 
+        {
+            var productTypes = _productTypeRepository.GetAll();
+            return _mapper.Map<List<ProductTypeDto>>(productTypes);
+        }
+
+        public ProductTypeDto Get(int id)
+        {
+            var productType = _productTypeRepository.Get(id);
+            return _mapper.Map<ProductTypeDto>(productType);
+        }
+
+        public ProductTypeDto Add(ProductTypeDto productTypeDto)
+        {
+            var productTypeModel = _mapper.Map<ProductType>(productTypeDto);
+
+            _productTypeRepository.Add(productTypeModel);
+
+            return productTypeDto;
+        }
+
+        public ProductTypeDto Update(ProductTypeDto productTypeDto)
+        {
+            var productTypeModel = _mapper.Map<ProductType>(productTypeDto);
+
+            _productTypeRepository.Update(productTypeModel);
+
+            return productTypeDto;
+        }
+
+        public ProductTypeDto Delete(int id) 
+        {
+            var deleted = _productTypeRepository.Delete(id);
+            return _mapper.Map<ProductTypeDto>(deleted);
+        }
+    }
+}
---
+++ ProgrammingClass3.AngularLesson/Services/Implementations/UnitOfMeasureService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using ProgrammingClass3.AngularLesson.DataTransferObjects;
+using ProgrammingClass3.AngularLesson.Models;
+using ProgrammingClass3.AngularLesson.Repositories.Definitions;
+
+namespace ProgrammingClass3.AngularLesson.Services.Implementations
+{
+    public class UnitOfMeasureService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfMeasureRepository _unitOfMeasureRepository;
+
+        public UnitOfMeasureService(IMapper mapper, IUnitOfMeasureRepository unitOfMeasureRepository)
+        {
+            _mapper = mapper;
+            _unitOfMeasureRepository = unitOfMeasureRepository;
+        }
+
+        public List<UnitOfMeasureDto> GetAll()
+        {
+            var unitOfMeasures = _unitOfMeasureRepository.GetAll();
+            return _mapper.Map<List<UnitOfMeasureDto>>(unitOfMeasures);
+        }
+
+        public UnitOfMeasureDto Get(int id)
+        {
+            var unitOfMeasure = _unitOfMeasureRepository.Get(id);
+            return _mapper.Map<UnitOfMeasureDto>(unitOfMeasure);
+        }
+
+        public UnitOfMeasureDto Add(UnitOfMeasureDto unitOfMeasureDto)
+        {
+            var unitOfMeasureModel = _mapper.Map<UnitOfMeasure>(unitOfMeasureDto);
+
+            _unitOfMeasureRepository.Add(unitOfMeasureModel);
+
+            return unitOfMeasureDto;
+        }
+
+        public UnitOfMeasureDto Update(UnitOfMeasureDto unitOfMeasureDto)
+        {
+            var unitOfMeasureModel = _mapper.Map<UnitOfMeasure>(unitOfMeasureDto);
+
+            _unitOfMeasureRepository.Update(unitOfMeasureModel);
+
+            return unitOfMeasureDto;
+        }
+
+        public UnitOfMeasureDto Delete(int id)
+        {
+            var deleted = _unitOfMeasureRepository.Delete(id);
+            return _mapper.Map<UnitOfMeasureDto>(deleted);
+        }
+    }
+}
